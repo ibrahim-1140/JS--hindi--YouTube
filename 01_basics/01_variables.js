e93e6563d4bde566379d16b9e07f519bb51240eb
Mohd Ibrahim Usmani <iusmani614@gmail.com>
2025-11-05 14:26:18 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId =144533
+let accountEmail = "zyx56@gmail.com"
+var accountPassword = "12345"
+accountCity = "jaipur"
+
+// accountId = 2 // not allowed
+accountEmail = "jgb2@gmail.com"
+accountPassword = "kkd"
+accountCity = "banguluru"
+
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
