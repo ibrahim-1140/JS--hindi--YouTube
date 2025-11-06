256f0f03f85326583cda3ff788603453a7a028e7
Mohd Ibrahim Usmani <iusmani614@gmail.com>
2025-11-06 14:36:18 +0000

    conversion of datatype

---
+++ 01_basics/03_conversation.js
@@ -0,0 +1,22 @@
+//let score = "33abc"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+//let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+//1 => true; 0 =>false
+//"" => false
+//"ibrahim" =>true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
